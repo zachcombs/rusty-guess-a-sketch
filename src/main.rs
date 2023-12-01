f839e2a70af12be1ed572db42ee79ed3d8400ea9
Zach Combs <zacharycombstwo@gmail.com>
2023-11-30 23:16:08 -0500

    Removed backslash

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ async fn main() -> std::io::Result<()> {
             //SWAP NEXT TWO LINES FOR LOCAL vs REMOTE
             // .allowed_origin("http://localhost:5173")
             .allowed_origin("https://guess-a-sketch-6hes.onrender.com")
-            .allowed_origin("https://www.guess-a-sketch.com/")
-            .allowed_origin("https://guess-a-sketch.com/")
+            .allowed_origin("https://www.guess-a-sketch.com")
+            .allowed_origin("https://guess-a-sketch.com")
             .allowed_origin("https://guess-a-sketch-git-develop-zachcombs-projects.vercel.app")
             // .allowed_origin_fn(|origin, _req_head| {
             //     origin.as_bytes().starts_with(b"https://guess-a-sketch")
