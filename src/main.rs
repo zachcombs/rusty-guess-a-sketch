f76f38ad0a90c74e2b539b528675dc97fa184c23
Zach Combs <zacharycombstwo@gmail.com>
2023-11-30 23:03:48 -0500

    add

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ async fn main() -> std::io::Result<()> {
             //SWAP NEXT TWO LINES FOR LOCAL vs REMOTE
             // .allowed_origin("http://localhost:5173")
             .allowed_origin("https://guess-a-sketch-6hes.onrender.com")
-            .allowed_origin("https://guess-a-sketch.com")
+            .allowed_origin("https://www.guess-a-sketch.com/")
+            .allowed_origin("https://guess-a-sketch.com/")
             .allowed_origin("https://guess-a-sketch-git-develop-zachcombs-projects.vercel.app")
-            .allowed_origin_fn(|origin, _req_head| {
-                origin.as_bytes().starts_with(b"https://guess-a-sketch")
-                    && origin.as_bytes().ends_with(b".vercel.app")
-            })
+            // .allowed_origin_fn(|origin, _req_head| {
+            //     origin.as_bytes().starts_with(b"https://guess-a-sketch")
+            //         && origin.as_bytes().ends_with(b".vercel.app")
+            // })
             .allowed_methods(vec!["GET"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
