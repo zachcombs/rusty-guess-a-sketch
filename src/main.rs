bdeb89f90cb6da14b5c6a530e7aa858c9d58e843
Zach Combs <zacharycombstwo@gmail.com>
2023-11-30 22:05:50 -0500

    Added AWS Lambda wrapper for rust web server

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,54 @@
 use actix_cors::Cors;
-use actix_web::{http, App, HttpServer};
+use actix_web::{App, HttpServer};
+use lambda_web::{is_running_on_lambda, run_actix_on_lambda};
 
 mod drawing;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        let cors = Cors::default()
-            //SWAP NEXT TWO LINES FOR LOCAL vs REMOTE
-            // .allowed_origin("http://localhost:5173")
-            .allowed_origin("https://guess-a-sketch-6hes.onrender.com")
-            .allowed_origin("https://guess-a-sketch-git-develop-zachcombs-projects.vercel.app")
-            .allowed_origin_fn(|origin, _req_head| {
-                origin.as_bytes().starts_with(b"http://guess-a-sketch")
-                    && origin.as_bytes().ends_with(b".vercel.app")
-            })
-            .allowed_methods(vec!["GET"])
-            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-            .allowed_header(http::header::CONTENT_TYPE)
-            .max_age(3600);
+    // let cors = Cors::default();
 
+    let factory = move || {
         App::new()
-            .wrap(cors)
+            .wrap(Cors::default())
             .service(drawing::drawing)
             .service(drawing::drawing_from_category)
-    })
-    //SWAP NEXT TWO LINES FOR LOCAL vs REMOTE
-    //.bind("127.0.0.1", 8080)
-    .bind(("0.0.0.0", 8088))?
-    .run()
-    .await
+    };
+
+    if is_running_on_lambda() {
+        let _ = run_actix_on_lambda(factory).await;
+    } else {
+        // Local server
+        HttpServer::new(factory)
+            .bind(("127.0.0.1", 8080))?
+            .run()
+            .await?;
+    }
+    Ok(())
+
+    // HttpServer::new(|| {
+    //     let cors = Cors::default()
+    //         //SWAP NEXT TWO LINES FOR LOCAL vs REMOTE
+    //         // .allowed_origin("http://localhost:5173")
+    //         .allowed_origin("https://guess-a-sketch-6hes.onrender.com")
+    //         .allowed_origin("https://guess-a-sketch-git-develop-zachcombs-projects.vercel.app")
+    //         .allowed_origin_fn(|origin, _req_head| {
+    //             origin.as_bytes().starts_with(b"http://guess-a-sketch")
+    //                 && origin.as_bytes().ends_with(b".vercel.app")
+    //         })
+    //         .allowed_methods(vec!["GET"])
+    //         .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+    //         .allowed_header(http::header::CONTENT_TYPE)
+    //         .max_age(3600);
+
+    //     App::new()
+    //         .wrap(cors)
+    //         .service(drawing::drawing)
+    //         .service(drawing::drawing_from_category)
+    // })
+    // //SWAP NEXT TWO LINES FOR LOCAL vs REMOTE
+    // //.bind("127.0.0.1", 8080)
+    // .bind(("0.0.0.0", 8088))?
+    // .run()
+    // .await
 }
